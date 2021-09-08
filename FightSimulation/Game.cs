3fef57850d823084d53d8b8c5fb482008b23e1b9
mikeLofton <michaellofton32@gmail.com>
2021-09-08 16:56:07 -0500

    Moved array exercise material

--- FightSimulation/Game.cs
+++ FightSimulation/Game.cs
@@ -28,8 +28,7 @@ namespace FightSimulation
 
         public void Run()
         {
-            int[] testArray = new int[] { 1, 2, 3, 4, 5 };
-            PrintNumbers(testArray, 5);
+            
 
             Start();
            
@@ -40,26 +39,6 @@ namespace FightSimulation
 
             End();
         }
-
-
-        void PrintNumbers(int[] numbers, int size)
-        {          
-
-            for (int i = 0; i < size; i++)
-            {
-                Console.WriteLine(numbers[i]);
-            }
-            Console.ReadKey();   
-        }
-
-        void PrintArray(int[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine(arr[i]);
-            }
-        }
-
 
 
         void Start()
