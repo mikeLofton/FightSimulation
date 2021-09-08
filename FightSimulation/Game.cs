9593e4d069e1a6e6a0dabd7f0c826451d5021466
mikeLofton <michaellofton32@gmail.com>
2021-09-08 16:13:03 -0500

    Array exercise

--- FightSimulation/Game.cs
+++ FightSimulation/Game.cs
@@ -24,8 +24,13 @@ namespace FightSimulation
         Monster backupWompus;
         Monster unclePhil;
 
+        
+
         public void Run()
         {
+            int[] testArray = new int[] { 1, 2, 3, 4, 5 };
+            PrintNumbers(testArray, 5);
+
             Start();
            
             while (!gameOver)
@@ -35,6 +40,27 @@ namespace FightSimulation
 
             End();
         }
+
+
+        void PrintNumbers(int[] numbers, int size)
+        {          
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadKey();   
+        }
+
+        void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+
 
         void Start()
         {
